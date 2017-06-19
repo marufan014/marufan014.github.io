bd41e1c72e153bf1cb312eea79dcec057c6b2527
marufan014 <marufankurnia@gmail.com>
2017-06-19 14:33:26 +0700

    Add files via upload

---
+++ QuizUAS2017/question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "3 + 4 = ?",
+	"option1": "'4'",
+	"option2": "'8'",
+	"option3": "'7'",
+	"option4": "'10'",
+	"answer": "3"
+}, {
+	"question": "13 + 8 = ?",
+	"option1": "23",
+	"option2": "44",
+	"option3": "21",
+	"option4": "10",
+	"answer": "3"
+}, {
+	"question": "Budi membeli permen sebanyak 12 butir. Lalu, Ani meminta permen Budi sebanyak 6 butir. Berapa sisa permen Budi ?",
+	"option1": "10 butir",
+	"option2": "6 butir",
+	"option3": "9 butir",
+	"option4": "10 butir",
+	"answer": "2"
+}, {
+	"question": "13 x 4 = ?",
+	"option1": "54",
+	"option2": "33",
+	"option3": "45",
+	"option4": "22",
+	"answer": "1"
+}, {
+	"question": "22 x 6 = ?",
+	"option1": "78",
+	"option2": "102",
+	"option3": "124",
+	"option4": "132",
+	"answer": "4"
+}, {
+	"question": "Ibu Budi membeli Anggur sebanyak 120 biji. Lalu dibagikan kepada Budi dan kedua adiknya. Berapakah banyak anggur yang didapat oleh setiap anak ??",
+	"option1": "50 biji",
+	"option2": "20 biji",
+	"option3": "40 biji",
+	"option4": "25 biji",
+	"answer": "3"
+}, {
+	"question": "238 - 115 = ?",
+	"option1": "234",
+	"option2": "123",
+	"option3": "121",
+	"option4": "100",
+	"answer": "2"
+}, {
+	"question": "467 + 238 = ?",
+	"option1": "665",
+	"option2": "772",
+	"option3": "802",
+	"option4": "705",
+	"answer": "4"
+}, {
+	"question": "23 x 26 = ?",
+	"option1": "348",
+	"option2": "867",
+	"option3": "598",
+	"option4": "450",
+	"answer": "3"
+}, {
+	"question": "138 : 23 = ?",
+	"option1": "8",
+	"option2": "12",
+	"option3": "6",
+	"option4": "4",
+	"answer": "3"
+}];
